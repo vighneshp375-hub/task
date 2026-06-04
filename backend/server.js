54730102274dad1f821392c4a30d675d6c56ed55
vighneshp375-hub <vighneshp375@gmail.com>
2026-06-04 12:35:37 +0530

    Final

--- backend/server.js
+++ backend/server.js
@@ -44,6 +44,15 @@ app.post('/login', async (req, res) => {
         }
         });
 
+app.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find();
+        res.status(200).json(tasks);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+});
+
 app.post('/tasks', async (req, res) => {
     try {
         const newTask = new Task(req.body);
